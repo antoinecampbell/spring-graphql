29ddfd748a4c270d6cd8f6f5931a4f45b927e8f8
Antoine Campbell <antoine.s.campbell@gmail.com>
2022-10-09 13:24:39 -0400

    Added mutations and example queries

--- src/main/kotlin/com/antoinecampbell/grapql/Item.kt
+++ src/main/kotlin/com/antoinecampbell/grapql/Item.kt
@@ -3,6 +3,9 @@ package com.antoinecampbell.grapql
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.data.repository.CrudRepository
 import org.springframework.graphql.data.GraphQlRepository
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.stereotype.Controller
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -10,10 +13,33 @@ import javax.persistence.Id
 
 @Entity
 data class Item(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long,
-    var name: String,
-    var description: String?
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    val name: String,
+    val description: String?
 )
+
+data class ItemInput(
+    val name: String,
+    val description: String?
+) {
+    fun toEntity() = Item(name = this.name, description = this.description)
+}
 
 @GraphQlRepository
 interface ItemRepository : CrudRepository<Item, Long>, QuerydslPredicateExecutor<Item>
+
+@Controller
+class ItemController(private val itemRepository: ItemRepository) {
+
+    @MutationMapping
+    fun addItem(@Argument item: ItemInput): Item {
+        return itemRepository.save(item.toEntity())
+    }
+
+    @MutationMapping
+    fun deleteItem(@Argument id: Long): Item? {
+        return itemRepository.findById(id)
+            .orElse(null)
+            ?.also { itemRepository.deleteById(id) }
+    }
+}
--- src/main/resources/db/test-data/R__test-data.sql
+++ src/main/resources/db/test-data/R__test-data.sql
@@ -6,3 +6,5 @@ INSERT INTO item (id, name, description)
 VALUES (1, 'item 1', 'first item'),
        (2, 'item 2', 'second item'),
        (3, 'item 3', 'third item');
+
+ALTER TABLE item ALTER COLUMN id RESTART WITH (SELECT (MAX(id) + 1) from item);
